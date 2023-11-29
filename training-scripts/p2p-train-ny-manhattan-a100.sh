69c3c3a76d42e8e404d27aa962978f2da53f7df6
Leonard Schrage <hello@leonardschrage.com>
2023-11-28 19:41:44 -0500

    Update p2p-train-ny-manhattan-a100.sh

--- training-scripts/p2p-train-ny-manhattan-a100.sh
+++ training-scripts/p2p-train-ny-manhattan-a100.sh
@@ -6,11 +6,11 @@
 ##SBATCH --gres=gpu:v100-sxm2:1
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=24GB
+#SBATCH --mem=32GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=l.schrage@northeastern.edu
 
 module load anaconda3/2022.05 cuda/11.8
 source activate /home/l.schrage/.conda/envs/re-blocking_env
 
-python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/ny-manhattan --checkpoints_dir /work/re-blocking/checkpoints --name ny-manhattan-100-60-a100 --model pix2pix --direction AtoB --n_epochs 100 --batch_size 60
+python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/ny-manhattan --checkpoints_dir /work/re-blocking/checkpoints --name ny-manhattan-100-60-a100-32gb --model pix2pix --direction AtoB --n_epochs 100 --batch_size 60
