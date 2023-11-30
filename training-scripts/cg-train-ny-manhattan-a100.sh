d955f486b8e22505a19013af22cc581903def37d
Leonard Schrage <hello@leonardschrage.com>
2023-11-30 18:26:49 -0500

    scripts with higher batch size

--- training-scripts/cg-train-ny-manhattan-a100.sh
+++ training-scripts/cg-train-ny-manhattan-a100.sh
@@ -13,4 +13,4 @@
 module load anaconda3/2022.05 cuda/11.8
 source activate /home/l.schrage/.conda/envs/re-blocking_env
 
-python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/ny-manhattan --checkpoints_dir /work/re-blocking/checkpoints --name ny-manhattan-cg-200-100-a100-32gb --model cycle_gan --direction AtoB --n_epochs 200 --batch_size 100
+python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/ny-manhattan --checkpoints_dir /work/re-blocking/checkpoints --name ny-manhattan-cg-200-10-a100-32gb --model cycle_gan --direction AtoB --n_epochs 200 --batch_size 10
