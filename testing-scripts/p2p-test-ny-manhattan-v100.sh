d0f1c36790f2d8318ddee0f0f1c0cb8f60b7c8a0
Demircan Tas <tasd@mit.edu>
2024-01-22 18:20:21 -0500

    temp branch for Jan 22 2024

---
+++ testing-scripts/p2p-test-ny-manhattan-v100.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=2:00:00
+#SBATCH --job-name=test-ny-m-v100
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100-sxm2:1
+##SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=10GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=l.schrage@northeastern.edu
+
+module load anaconda3/2022.05 cuda/11.8
+source activate /home/l.schrage/.conda/envs/re-blocking_env
+
+python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/test.py --dataroot /work/re-blocking/data/ny-brooklyn --checkpoints_dir /work/re-blocking/checkpoints --name ny-manhattan-200-150-v100 --model pix2pix
---
+++ training-scripts/p2p-train-ny-manhattan-v100.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=8:00:00
+#SBATCH --job-name=train-ny-m-p2p-v100
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100-sxm2:1
+##SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=l.schrage@northeastern.edu
+
+module load anaconda3/2022.05 cuda/11.8
+source activate /home/l.schrage/.conda/envs/re-blocking_env
+
+python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/ny-manhattan --checkpoints_dir /work/re-blocking/checkpoints --name ny-manhattan-p2p-200-150-v100 --model pix2pix --direction AtoB --save_epoch_freq 1 --continue_train --epoch_count 64 --n_epochs 200 --batch_size 150
