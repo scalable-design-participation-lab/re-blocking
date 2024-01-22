89cbeb9d2a51a86a0f6b39bc6992f5fd19e54bee
Leonard Schrage <hello@leonardschrage.com>
2024-01-21 22:49:43 -0500

    continued

--- training-scripts/p2p-train-nc-charlotte-v100.sh
+++ training-scripts/p2p-train-nc-charlotte-v100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=5:00:00
+#SBATCH --time=8:00:00
 #SBATCH --job-name=train-nc-c-p2p-v100
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100-sxm2:1
@@ -13,4 +13,4 @@
 module load anaconda3/2022.05 cuda/11.8
 source activate /home/l.schrage/.conda/envs/re-blocking_env
 
-python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/nc-charlotte --checkpoints_dir /work/re-blocking/checkpoints --name nc-charlotte-200-150-v100 --model pix2pix --direction AtoB --save_epoch_freq 1 --continue_train --epoch_count 19 --n_epochs 200 --batch_size 150
+python3 /work/re-blocking/pytorch-CycleGAN-and-pix2pix/train.py --dataroot /work/re-blocking/data/nc-charlotte --checkpoints_dir /work/re-blocking/checkpoints --name nc-charlotte-200-150-v100 --model pix2pix --direction AtoB --save_epoch_freq 1 --continue_train --epoch_count 25 --n_epochs 200 --batch_size 150
