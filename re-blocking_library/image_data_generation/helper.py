c8b2a5f7ef95e7f535bc27c9febd96c74b9cb07e
Katerina Labrou <41840736+klabrou@users.noreply.github.com>
2024-02-23 11:28:18 -0500

    First Commit - Image data generation

---
+++ re-blocking_library/image_data_generation/helper.py
@@ -0,0 +1,266 @@
+import random
+import geopandas as gpd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from cartopy.io.img_tiles  import MapboxTiles
+import cartopy.crs as ccrs
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+def random_hex_color(seed=False):
+  if seed:
+    random.seed(seed)
+    r = random.randint(0, 255)
+    random.seed(seed+1000)
+    g = random.randint(0, 255)
+    random.seed(seed+2000)
+    b = random.randint(0, 255)
+  else:
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+  return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+def remove_duplicates(df):
+    indexes_to_remove = []
+    my_dict = {}
+    for index, row in tqdm(df.iterrows()):
+        if row['geometry'] not in my_dict:
+            my_dict[row['geometry']] = 1
+        else:
+            indexes_to_remove.append(index)
+    df = df.drop(indexes_to_remove)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+# loop through the parcels and add them to the map
+def add_geometries(ax, df_parcels, crs_epsg, random_color = False):
+    for row in df_parcels.itertuples():
+        geometry = row.geometry
+        if random_color == True: color = random_hex_color(int(row.bin))
+        else: color = row.color
+        ax.add_geometries(geometry, crs = crs_epsg, facecolor=color) # for Lat/Lon data.
+        
+# subset the data frames based on a buffer
+def subset(df, df_buildings, index, distance = 75):
+    selected_feature = df.loc[index]
+    geometry_buffer = selected_feature.geometry.buffer(distance)
+    geometry_bounds = selected_feature.geometry.buffer(distance-70)
+
+    return df[df.within(geometry_buffer)], df_buildings[df_buildings.within(geometry_buffer)], geometry_bounds.bounds
+        
+def map_maker(df_parcels, df_buildings, bounds, index, scale=10, feature_type='both', random_color=False, img_folder = None, epsg = None):
+    access_token = os.environ.get('MAPBOX_ACCESS_TOKEN')
+    tiler = MapboxTiles(access_token, 'satellite-v9')
+    crs_epsg = ccrs.epsg('3857')
+
+    mercator = tiler.crs
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection=mercator)
+
+    # change figure size of the subplot
+    my_dpi=96
+    fig.set_size_inches(7, 7)
+    # fig.figsize = (512/my_dpi, 512/my_dpi), dpi=my_dpi
+
+    # calculate the centroid and max distance of the bounds
+    dist1 = bounds[2]-bounds[0]
+    dist2 = bounds[3]-bounds[1]
+    max_dist = max(dist1, dist2)/2
+
+    # calculate the centroid of the bounds
+    centroid_x = (bounds[2]+bounds[0])/2
+    centroid_y = (bounds[3]+bounds[1])/2
+
+    # bounds = df_parcels.total_bounds with offset to create same aspect ratio
+    ax.set_extent([centroid_x-max_dist, centroid_x+max_dist, centroid_y-max_dist, centroid_y+max_dist], crs=ccrs.epsg('3857'))
+    # ax.set_extent([centroid_x-max_dist, centroid_x+max_dist, centroid_y-max_dist, centroid_y+max_dist], crs=ccrs.epsg('3857'))
+
+    # if feature_type == 'parcels': add_parcels(ax, df_parcels, crs_epsg)
+    if feature_type == 'parcels': 
+        add_geometries(ax, df_parcels, crs_epsg)
+        # ax.add_geometries(df_buildings.geometry, crs = crs_epsg, facecolor='white', edgecolor='black', linewidth=2.5, alpha=1)
+    if feature_type == 'parcels' and random_color == True:
+        add_geometries(ax, df_parcels, crs_epsg, random_color=True)
+    if feature_type == 'buildings': 
+        add_geometries(ax, df_buildings, crs_epsg)
+    if feature_type == 'buildings' and random_color == True:
+        add_geometries(ax, df_buildings, crs_epsg, random_color=True)
+    if feature_type == 'both' and random_color == True: 
+        add_geometries(ax, df_buildings, crs_epsg, random_color=True)
+        add_geometries(ax, df_parcels, crs_epsg, random_color=True)
+    if  feature_type == 'both': 
+        # ax.add_geometries(df_buildings.geometry, crs = crs_epsg, facecolor='black', edgecolor='white', linewidth=1.5, alpha=1)
+        add_geometries(ax, df_buildings, crs_epsg, random_color=True)
+        add_geometries(ax, df_parcels, crs_epsg)
+
+    # add the Mapbox tiles to the axis at zoom level 10 (Mapbox has 23 zoom levels)
+    ax.add_image(tiler, scale)
+
+    # save the figure
+    plt.savefig(img_folder + f'{feature_type}_{index}.jpg', bbox_inches='tight', pad_inches = 0, dpi = my_dpi)
+    
+    # close the figure
+    plt.close(fig)
+
+    # ax.coastlines('10m')
+    plt.show()
+    
+def split_building(buildings_with_multiple_parcels, buildings_with_parcel_info, df_buildings, df_parcels, 
+                   threshold_high = 0.75, threshold_low = 0.15, buildings_clean_path = './'):
+    
+    # Create an empty GeoDataFrame to hold the split buildings
+    split_buildings = gpd.GeoDataFrame()
+
+    # Initialize a counter for tracking progress
+    count = 0
+
+    # Create a dictionary to store information about parcels to be removed
+    to_remove = {}
+
+    # Iterate through buildings with multiple parcels
+    print("Iterate through buildings in multiple parcels and split them. Splitting...")
+    for building_id in tqdm(buildings_with_multiple_parcels):
+        # Print progress every 1000 buildings
+        if count % 1000 == 0:
+            print(count)
+        
+        # Increment the counter
+        count += 1
+        
+        # Select the building with the current building_id
+        building = df_buildings[df_buildings["building_id"] == building_id].copy()
+        
+        # Get the list of parcel IDs that intersect with the building
+        parcel_ids = buildings_with_parcel_info[buildings_with_parcel_info["building_id"] == building_id]["parcel_id"].tolist()
+
+        # Initialize lists to store split geometries and areas
+        split_geometries = []
+        areas = []
+        
+        # Split the building geometry into separate parts based on the parcels
+        i = 0
+        for parcel_id in parcel_ids:
+            split_geometries.append(df_parcels[df_parcels["parcel_id"] == parcel_id].geometry.intersection(building.geometry.unary_union)) 
+            areas.append(split_geometries[i].area.values[0])
+            i += 1
+            
+        # Calculate the normalized areas for each parcel
+        areas_normalized = [a/sum(areas) for a in areas]
+        
+        # Find the maximum normalized area value and its index
+        max_value = max(areas_normalized)
+        max_index = areas_normalized.index(max_value)
+        
+        # If the maximum area is greater than or equal to threshold_high, remove everything except the one with high area
+        if max_value >= threshold_high:
+            to_remove[building_id] = parcel_ids[:max_index] + parcel_ids[max_index + 1:]
+        else:
+            # If the maximum area is less than threshold_high, remove all parcels below 0.15 and between 0.15 and threshold_high
+            for i in range(len(areas_normalized)):
+                if areas_normalized[i] <= threshold_low:
+                    if building_id in to_remove:
+                        to_remove[building_id] = to_remove[building_id] + [parcel_ids[i]]
+                    else:
+                        to_remove[building_id] = [parcel_ids[i]]
+            for i in range(len(areas_normalized)):
+                if threshold_low < areas_normalized[i] < threshold_high:
+                    if building_id in to_remove:
+                        to_remove[building_id] = to_remove[building_id] + [parcel_ids[i]]
+                    else:
+                        to_remove[building_id] = [parcel_ids[i]]
+                    
+                    # Create a temporary building with split geometry and update its attributes
+                    building_temp = buildings_with_parcel_info[(buildings_with_parcel_info["building_id"] == building_id) & (buildings_with_parcel_info["parcel_id"] == parcel_ids[i])].copy()
+                    building_temp.geometry = split_geometries[i].values
+                    building_temp['building_id'] = str(building_temp['building_id'].values[0]) + '_' + str(i)
+                    
+                    # Append the temporary building to the split_buildings GeoDataFrame
+                    split_buildings = split_buildings.append(building_temp, ignore_index=True)
+                    
+    buildings_with_parcel_info_new = buildings_with_parcel_info.copy()
+    print("Add the new buildings' geometries...")
+    for key, values in tqdm(to_remove.items()):
+        for v in values:
+            buildings_with_parcel_info_new = buildings_with_parcel_info_new[(buildings_with_parcel_info_new['building_id']!=key) | (buildings_with_parcel_info_new['parcel_id']!=v) ]
+    # print(buildings_with_parcel_info_new.shape[0])
+    # add new ones
+    buildings_with_parcel_info_new = buildings_with_parcel_info_new.append(split_buildings, ignore_index=True)
+    buildings_with_parcel_info_new['building_id'] = buildings_with_parcel_info_new['building_id'].astype(str)
+
+    #save geopandas
+    # buildings_with_parcel_info_new.to_file(buildings_clean_path, driver='GeoJSON')
+    buildings_with_parcel_info_new.to_file(buildings_clean_path, driver='ESRI Shapefile')
+    
+    return buildings_with_parcel_info_new
+
+
+def generate_dataset_specs(buildings_with_parcel_info, buildings_with_parcel_info_new, df_parcels):
+    print('Generating dataset specs...')
+    
+    # Calculate number of buildings before/after split
+    buildings_before = buildings_with_parcel_info.shape[0]
+    buildings_after = buildings_with_parcel_info_new.shape[0]
+    number_of_parcels = df_parcels.shape[0]
+    
+    # Spatial join to associate each building with a parcel
+    joined_df = gpd.sjoin(buildings_with_parcel_info_new, df_parcels, op='within', how='left')
+    
+    # Group by parcel ID and count buildings
+    building_counts_per_parcel = joined_df.groupby('index_right').size()
+    
+    # Filter parcels with more than one building
+    parcels_with_multiple_buildings = building_counts_per_parcel[building_counts_per_parcel > 1]
+    num_parcels_with_multiple_buildings = len(parcels_with_multiple_buildings)
+    
+    # Calculate parcel areas
+    df_parcels['area'] = df_parcels.geometry.area
+    
+    # Plot histogram of parcel areas
+    plt.figure(figsize=(10, 6))
+    plt.hist(df_parcels['area'], bins=50, color='skyblue', edgecolor='black')
+    plt.title('Parcel Area Distribution')
+    plt.xlabel('Area (square meters)')
+    plt.ylabel('Number of Parcels')
+    plt.grid(True)
+    plt.savefig('./dataset_specs/parcel_area_distribution.png', dpi=300)
+    plt.show()
+    
+    # Generate descriptive statistics for the 'area' column
+    area_description = df_parcels['area'].describe()
+    # area_description.to_csv('./dataset_specs/parcel_area_statistics.csv')
+    
+    # Spatial join to associate buildings with parcels for coverage calculation
+    joined_df = gpd.sjoin(buildings_with_parcel_info_new, df_parcels, how='inner', op='intersects')
+    joined_df['intersection_area'] = joined_df.apply(lambda row: row['geometry'].intersection(df_parcels.loc[row['index_right']].geometry).area, axis=1)
+    building_area_per_parcel = joined_df.groupby('index_right')['intersection_area'].sum()
+    
+    # Calculate parcel areas and coverage percentage
+    df_parcels['parcel_area'] = df_parcels.geometry.area
+    df_parcels = df_parcels.join(building_area_per_parcel, on=df_parcels.index, rsuffix='_building')
+    df_parcels['building_area'] = df_parcels['intersection_area'].fillna(0)
+    df_parcels['coverage_percentage'] = (df_parcels['building_area'] / df_parcels['parcel_area']) * 100
+    
+    # Generate descriptive statistics for the 'coverage_percentage' column
+    coverage_description = df_parcels['coverage_percentage'].describe()
+    # coverage_description.to_csv('./dataset_specs/parcel_coverage_statistics.csv')
+    
+    with open('./dataset_specs/dataset_summary_and_statistics.txt', 'w') as file:
+        # Save initial summary information
+        file.write(f'Buildings before: {buildings_before}\n')
+        file.write(f'Buildings after: {buildings_after}\n')
+        file.write(f'Number of parcels: {number_of_parcels}\n')
+        file.write(f'Parcels with multiple buildings: {num_parcels_with_multiple_buildings}\n\n')
+        
+        # Save area statistics
+        file.write('Area Statistics:\n')
+        area_stats = area_description.to_string()  # Convert the area description DataFrame/Series to a string
+        file.write(area_stats)
+        file.write('\n\n')  # Add some space before the next section
+        
+        # Save coverage statistics
+        file.write('Coverage Statistics:\n')
+        coverage_stats = coverage_description.to_string()  # Convert the coverage description DataFrame/Series to a string
+        file.write(coverage_stats)
---
+++ re-blocking_library/image_data_generation/main.py
@@ -0,0 +1,150 @@
+# import libraries
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from cartopy.io.img_tiles import MapboxTiles
+import geopandas as gpd
+import os
+from dotenv import load_dotenv
+load_dotenv() # loads .env file from main folder for Mapbox API key and local project path
+import random
+import numpy as np
+import shapely
+
+from helper import *
+
+import argparse
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='This is a script to generate two image datasets, parcel_images and one for building_images, from the respective shp or geoJSON data.')
+
+    # Add command-line arguments
+    parser.add_argument("--buildings_path", help ='Path to buildings (shapefile or geoJSON)', type=str, required=True)
+    parser.add_argument("--parcels_path", help ='Path to parcels (shapefile or geoJSON)', type=str, required=True)
+    parser.add_argument("--blocks_path", help ='Path to blocks (shapefile or geoJSON)', type=str, required=False, default=None)
+    parser.add_argument("--split_buildings", help ='Whether to split buildings', type=bool, required=True, default=True)
+    parser.add_argument("--threshold_high", help = (
+            "A decimal value between 0 and 1 representing the high building-parcel overlap threshold. "
+            "If the ratio of building area in a parcel to the total building area is greater than or "
+            "equal to the specified threshold, the building is assigned to the parcel."), type=float, default = 0.75, required=False)
+    parser.add_argument("--threshold_low", help =(
+            "A decimal value between 0 and 1 representing the building-parcel overlap threshold. "
+            "If the ratio of building area in a parcel to the total building area is less than or "
+            "equal to the specified threshold, the overlap is considered insignificant and will "
+            "be overlooked."),type=float, default = 0.15, required=False)
+    parser.add_argument("--buildings_w_parcels_path", help ='buildings_with_parcels_path', type=str, required=True) # to save shp
+    parser.add_argument("--parcel_images_directory", help ='parcel_images_directory', type=str, required=True)
+    parser.add_argument("--buildings_images_directory", help ='buildings_images_directory', type=str, required=True)
+    parser.add_argument("--number_of_images", help ='number_of_images', type=int, default = 25000, required=True)
+    
+    
+    # create dataset_specs folder
+    try:
+        os.mkdir('./dataset_specs')
+    except:
+        pass
+    
+    
+    # retrieve arguments
+    args = parser.parse_args()
+    buildings_path = args.buildings_path
+    parcels_path = args.parcels_path
+    threshold_high = args.threshold_high
+    threshold_low = args.threshold_low
+    buildings_w_parcels_path = args.buildings_w_parcels_path
+    parcel_images_directory = args.parcel_images_directory
+    buildings_images_directory = args.buildings_images_directory
+    number_of_images = args.number_of_images
+    blocks_path = args.blocks_path
+    split_buildings = args.split_buildings
+    
+    # read dataframes
+    df_parcels = gpd.read_file(parcels_path).to_crs(epsg=3857)
+    df_buildings = gpd.read_file(buildings_path).to_crs(epsg=3857)
+    if blocks_path is not None:
+        df_blocks = gpd.read_file(blocks_path).to_crs(epsg=3857)
+    
+    # set color to parcels
+    df_parcels['color'] = [ random_hex_color() for i in range(len(df_parcels)) ]
+    
+    # remove duplicates from buildings and parcels
+    print("Removing duplicates buildings...")
+    df_buildings = remove_duplicates(df_buildings)
+    print("Removing duplicates parcels...")
+    df_parcels = remove_duplicates(df_parcels)
+    if blocks_path is not None:
+        print("Removing duplicates blocks...")
+        df_blocks = remove_duplicates(df_blocks)
+        # Spatial join to restrict parcels to those within blocks
+        df_parcels = gpd.sjoin(df_parcels, df_blocks, op='within')
+        try:
+            df_parcels.drop(columns = ['index_left','index_right'], inplace=True)
+        except:
+            pass
+        # Reset the index of the resulting DataFrame
+        df_parcels.reset_index(inplace=True, drop=True)
+        
+    # add building id and parcel id
+    df_buildings['building_id'] = [i for i in range(len(df_buildings))]
+    df_parcels['parcel_id'] = [i for i in range(len(df_parcels))]
+    
+    # Perform a spatial join between buildings and parcels
+    buildings_with_parcel_info = df_buildings.sjoin(df_parcels, how="inner")
+    
+    # Count the number of times each building appears (number of intersecting parcels)
+    building_counts = buildings_with_parcel_info.groupby("building_id").size()
+    #Find buildings that belong to more than one parcel
+    buildings_with_multiple_parcels = building_counts[building_counts > 1].index.tolist()
+   
+    
+    # split buildings based on the provided thresholds
+    if split_buildings:
+        buildings_with_parcel_info_new = split_building(buildings_with_multiple_parcels, buildings_with_parcel_info, df_buildings, df_parcels, 
+                   threshold_high = threshold_high, threshold_low = threshold_low, buildings_clean_path = buildings_w_parcels_path)
+    else:
+        buildings_with_parcel_info_new = buildings_with_parcel_info.copy()
+        
+    ################### DATASET SPECS ###################
+    generate_dataset_specs(buildings_with_parcel_info, buildings_with_parcel_info_new, df_parcels)
+    
+    ####### ASSIGN COLORS on buildings based on parcels
+    print('Assigning colors...')
+    for index, row in tqdm(buildings_with_parcel_info_new.iterrows()):
+        parcel_id = row['parcel_id']
+        buildings_with_parcel_info_new.loc[index,'color'] = df_parcels[df_parcels['parcel_id']==parcel_id].color.values[0]
+    # Save final shp with colors
+    buildings_with_parcel_info_new.to_file(buildings_w_parcels_path, driver='ESRI Shapefile')
+    
+    
+    ################ IMAGE DATASETS GENERATION ################ 
+    # Create directories to save images names
+    try:
+        os.mkdir(parcel_images_directory)
+    except:
+        pass
+    try:
+        os.mkdir(buildings_images_directory)
+    except:
+        pass
+    
+    # shuffle building indices - image index will correspond to the parcel index in the dataframe
+    indices_to_print = range(buildings_with_parcel_info_new.shape[0])
+    random.shuffle(indices_to_print)
+    count = 0 
+    print('Generating images...')
+    try:
+        for i in indices_to_print:
+            if count % 1000 == 0:
+                print(count+' images completed out of '+str(number_of_images))
+            subset_features = subset(df_parcels, buildings_with_parcel_info_new, i, 200)
+            map_maker(subset_features[0], subset_features[1], subset_features[2], i, 18, 'parcels', img_folder=parcel_images_directory)
+            map_maker(subset_features[0], subset_features[1], subset_features[2], i, 18, 'buildings', img_folder=buildings_images_directory)
+            count = count + 1
+            if count == number_of_images:
+                break
+    except:
+        print("Error at index: ",i)
+        
+    print('Done!')
+        
+    
