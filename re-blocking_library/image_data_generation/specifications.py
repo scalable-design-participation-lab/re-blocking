c19aeab2eb34de5af566f2d81e03d06c69f77e91
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2024-04-08 22:20:10 -0400

    feat(specifications): upgraded specifications

--- re-blocking_library/image_data_generation/specifications.py
+++ re-blocking_library/image_data_generation/specifications.py
@@ -5,36 +5,73 @@ A library for generating and validating parcel data for informal settlements
 
 class Parcels:
     """🐕"""
-    name: str
-    """The name of our dog."""
-    friends: list["Dog"]
-    """The friends of our dog."""
+    parcel_path: str
+    """Location of the parcel SHP file."""
+    buildings_path: str
+    """Location of the buildings SHP file."""
+    blocks_path: str
+    """Location of the block SHP file."""
 
-    def __init__(self, name: str):
-        """Make a Dog without any friends (yet)."""
-        self.name = name
-        self.friends = []
+    def __init__(self, parcel_path: str, buildings_path: str, blocks_path: str or None = None):
+        """Create a parcel block geodataframe."""
+        self.parcel_path = parcel_path
+        self.buildings_path = buildings_path
+        self.blocks_path = blocks_path
 
-    def bark(self, loud: bool = True):
-        """*woof*"""
+    # def bark(self, loud: bool = True):
+    #     """*woof*"""
 
-Read dataframes
-Set color to parcels
-Remove duplicates from buildings and parcels
-Spatial join to restrict parcels to those within blocks
-Reset the index of the resulting DataFrame
-Add building id and parcel id
-Perform a spatial join between buildings and parcels
-Count the number of times each building appears (number of intersecting parcels)
-Find buildings that belong to more than one parcel
-Split buildings based on the provided thresholds
-DATASET SPECS
-ASSIGN COLORS on buildings based on parcels
-Save final shp with colors
-IMAGE DATASETS GENERATION
-Create directories to save images names
-Shuffle building indices - image index will correspond to the parcel index in the data frame
-Generate Images
+Functions:
+Every numbered element below is a function that can be called by the Parcel object.
+Helper functions are now functions that are part of the class.
+1- Init:
+    Create df's based on the paths provided 
+    If blocks_path is provided, restrict parcels to those within blocks
+    Make sure that building and parcel id is the same by joining the two dataframes
+    Remove duplicates from buildings and parcels
+
+2- Split buildings:
+    Find buildings that belong to more than one parcel
+    Split buildings based on the provided thresholds
+    Update the building dataframe with the split buildings
+
+3- Assign colors:
+    Set color to parcels
+    This will depend on whether the parcels are split or not, if they are split, the color will be assigned to the split parcels
+
+4- Generate dataset specs:
+    Count the number of times each building appears (number of intersecting parcels)
+    Generates a set of specitications for the parcels and buildings (sort of what generate_dataset_specs does)
+
+5- Generate images:
+    Create directories to save buildings and parcels with buildings
+    Generate Images
+    
+6- random_hex_color:
+    Generate a random hex color
+    
+7- remove_duplicates:
+    Remove duplicates from a dataframe
+    
+8- add geometries:
+    Add geometries to a map plot
+
+9- plot a map 
+    calculate the centroid and max distance of the bounds
+    Plot a map with parcels and buildings or just with Parcels
+    save the image
+    
+10- split_building:
+    split a building when it falls within multiple parcels based on the provided thresholds
+    returns the split buildings dataframe
+    
+11- generate_dataset_specs:
+    Generate a set of specifications for the parcels and buildings
+    creates plots with histograms
+    creates a summary of the data as a text file
+    
+    
+    
 
 
 ## Helper
