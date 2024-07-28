caa60d91bbaeb2878be0ae47569777294ad4e4e5
Leonard Schrage <lschrage@mit.edu>
2024-07-28 10:20:52 +0200

    Update p2p-train-combined-v100.sh

--- training-scripts/p2p-train-combined-v100.sh
+++ training-scripts/p2p-train-combined-v100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=8:00:00
-#SBATCH --job-name=train-ma-b-p2p-v100
+#SBATCH --job-name=train-comb-p2p-v100
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100-sxm2:1
 ##SBATCH --gres=gpu:a100:1
