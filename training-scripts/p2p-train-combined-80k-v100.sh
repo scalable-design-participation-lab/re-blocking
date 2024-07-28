591d75e1fe847123bab3f4425c10ec8eabfbaeb8
Leonard Schrage <lschrage@mit.edu>
2024-07-28 10:27:41 +0200

    training for new combined model with less input images

--- training-scripts/p2p-train-combined-80k-v100.sh
+++ training-scripts/p2p-train-combined-80k-v100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=8:00:00
-#SBATCH --job-name=train-comb-p2p-v100
+#SBATCH --job-name=train-comb-80k-p2p-v100
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100-sxm2:1
 #SBATCH --cpus-per-task=4
